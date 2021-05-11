7508ec7c8315595c1a6c81f026e09065bad71c5f
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-11 19:52:04 -0300

    Importing npm modules

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
+const validator = require('validator');
 const utils = require('./utils');
 const getNotes = require('./notes');
 
-fs.writeFileSync('notes-app/notes.txt', 'This file was created by nodejs.\n');
-fs.appendFileSync('notes-app/notes.txt', 'This is the new message.\n')
+fs.writeFileSync('notes.txt', 'This file was created by nodejs.\n');
+fs.appendFileSync('notes.txt', 'This is the new message.\n')
 
 const name = 'Werison';
 console.log(name);
@@ -11,3 +12,6 @@ console.log(utils.age);
 
 console.log(utils.add(6, 7));
 console.log(getNotes());
+
+console.log(validator.isEmail('werison@email.com'));
+console.log(validator.isURL('http://url.com'));
