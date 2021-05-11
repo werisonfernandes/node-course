360e22853a7e283d9e14ffc6c5bceba4d1d5f8e2
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-11 19:23:33 -0300

    Import your own files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,13 @@
 const fs = require('fs');
+const utils = require('./utils');
+const getNotes = require('./notes');
 
 fs.writeFileSync('notes-app/notes.txt', 'This file was created by nodejs.\n');
 fs.appendFileSync('notes-app/notes.txt', 'This is the new message.\n')
+
+const name = 'Werison';
+console.log(name);
+console.log(utils.age);
+
+console.log(utils.add(6, 7));
+console.log(getNotes());
---
+++ notes-app/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js');
+
+const age = 38;
+
+const add = function(a, b) {
+    return a + b;
+}
+
+module.exports = { age, add };
