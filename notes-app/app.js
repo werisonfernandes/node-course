d43f2f0db2de5af3c25d929e8b04acc9436ba63b
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-12 09:34:36 -0300

    Printing colors

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const validator = require('validator');
+const chalk =  require('chalk');
+
 const utils = require('./utils');
 const getNotes = require('./notes');
 
@@ -15,3 +17,5 @@ console.log(getNotes());
 
 console.log(validator.isEmail('werison@email.com'));
 console.log(validator.isURL('http://url.com'));
+
+console.log(chalk.green.inverse.bold('Success.'));
