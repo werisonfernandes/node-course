7662f3d0ea69fb6041663e3eb89c1a6eea1b06fb
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-12 19:09:07 -0300

    Asynchronous basic

---
+++ weather-app/app.js
@@ -0,0 +1,25 @@
+console.log('Starting')
+
+setTimeout(() => {
+    console.log('2 Second Timer')
+}, 2000)
+
+setTimeout(() => {
+    console.log('0 Second Timer')
+}, 0)
+
+console.log('Process')
+
+console.log('Process')
+
+console.log('Process')
+
+console.log('Process')
+
+console.log('Process')
+
+console.log('Process')
+
+console.log('Process')
+
+console.log('Stopping')
