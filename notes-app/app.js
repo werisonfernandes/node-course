8776f1555d57bb3e324395c3f1fbe12ca3602632
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-12 11:31:18 -0300

    Argument parsing with yargs. part-1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const validator = require('validator');
 const chalk =  require('chalk');
+const yargs = require('yargs');
 
 const utils = require('./utils');
 const getNotes = require('./notes');
+const { describe } = require('yargs');
 
 fs.writeFileSync('notes.txt', 'This file was created by nodejs.\n');
 fs.appendFileSync('notes.txt', 'This is the new message.\n')
@@ -31,3 +33,48 @@ if(command === 'add') {
 } else if(command == 'remove') {
     console.log('User input: Command removed: ', process.argv[2]);
 }
+
+//Yargs parsing argv
+console.log(yargs.argv);
+
+//Costumize yargs version
+yargs.version('1.0.0');
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    handler: function() {
+        console.log('Adding a new note');
+    }
+});
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note');
+    }
+});
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Listing all notes');
+    }
+});
+
+//Create search command
+yargs.command({
+    command: 'search',
+    describe: 'Search a note',
+    handler: function() {
+        console.log('Searching a note');
+    }
+});
+
+//Yargs parsing argv
+console.log(yargs.argv);
