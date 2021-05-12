2233010710ed12e55f348812c328e3cb4d62e5c7
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-12 14:14:35 -0300

    Adding a note

---
+++ notes-app/1.app.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const validator = require('validator');
+const chalk =  require('chalk');
+const yargs = require('yargs');
+
+const utils = require('./utils');
+const getNotes = require('./notes');
+const { describe } = require('yargs');
+
+fs.writeFileSync('notes.txt', 'This file was created by nodejs.\n');
+fs.appendFileSync('notes.txt', 'This is the new message.\n')
+
+const name = 'Werison';
+console.log(name);
+console.log(utils.age);
+
+console.log(utils.add(6, 7));
+console.log(getNotes());
+
+console.log(validator.isEmail('werison@email.com'));
+console.log(validator.isURL('http://url.com'));
+
+console.log(chalk.red.inverse.bold('Error!!!'));
+console.log(chalk.green.inverse.bold('Success.'));
+
+//Input via command line
+console.log('User input: ', process.argv);
+
+//Input via command line. Specific position of array.
+const command = process.argv[2];
+if(command === 'add') {
+    console.log('User input: Command added: ', process.argv[2]);
+} else if(command == 'remove') {
+    console.log('User input: Command removed: ', process.argv[2]);
+}
+
+//Yargs parsing argv
+//console.log(yargs.argv);
+
+//Costumize yargs version
+yargs.version('1.0.0');
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            //To be required
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            //To be required
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Adding a new note: ', argv.title + argv.body);
+    }
+});
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing a note');
+    }
+});
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function() {
+        console.log('Listing all notes');
+    }
+});
+
+//Create search command
+yargs.command({
+    command: 'search',
+    describe: 'Search a note',
+    handler: function() {
+        console.log('Searching a note');
+    }
+});
+
+//Yargs parsing argv
+//console.log(yargs.argv);
+yargs.parse();
--- notes-app/app.js
+++ notes-app/app.js
@@ -1,95 +1,56 @@
-const fs = require('fs');
-const validator = require('validator');
-const chalk =  require('chalk');
-const yargs = require('yargs');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const notes = require('./notes.js')
 
-const utils = require('./utils');
-const getNotes = require('./notes');
-const { describe } = require('yargs');
+// Customize yargs version
+yargs.version('1.1.0')
 
-fs.writeFileSync('notes.txt', 'This file was created by nodejs.\n');
-fs.appendFileSync('notes.txt', 'This is the new message.\n')
-
-const name = 'Werison';
-console.log(name);
-console.log(utils.age);
-
-console.log(utils.add(6, 7));
-console.log(getNotes());
-
-console.log(validator.isEmail('werison@email.com'));
-console.log(validator.isURL('http://url.com'));
-
-console.log(chalk.red.inverse.bold('Error!!!'));
-console.log(chalk.green.inverse.bold('Success.'));
-
-//Input via command line
-console.log('User input: ', process.argv);
-
-//Input via command line. Specific position of array.
-const command = process.argv[2];
-if(command === 'add') {
-    console.log('User input: Command added: ', process.argv[2]);
-} else if(command == 'remove') {
-    console.log('User input: Command removed: ', process.argv[2]);
-}
-
-//Yargs parsing argv
-//console.log(yargs.argv);
-
-//Costumize yargs version
-yargs.version('1.0.0');
-
-//Create add command
+// Create add command
 yargs.command({
     command: 'add',
-    describe: 'Add a note',
+    describe: 'Add a new note',
     builder: {
         title: {
             describe: 'Note title',
-            //To be required
             demandOption: true,
             type: 'string'
         },
         body: {
             describe: 'Note body',
-            //To be required
             demandOption: true,
             type: 'string'
         }
     },
-    handler: function(argv) {
-        console.log('Adding a new note: ', argv.title + argv.body);
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
-});
+})
 
-//Create remove command
+// Create remove command
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function() {
-        console.log('Removing a note');
+    handler: function () {
+        console.log('Removing the note')
     }
-});
+})
 
-//Create list command
+// Create list command
 yargs.command({
     command: 'list',
-    describe: 'List all notes',
-    handler: function() {
-        console.log('Listing all notes');
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
     }
-});
+})
 
-//Create search command
+// Create read command
 yargs.command({
-    command: 'search',
-    describe: 'Search a note',
-    handler: function() {
-        console.log('Searching a note');
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
     }
-});
+})
 
-//Yargs parsing argv
-//console.log(yargs.argv);
-yargs.parse();
+yargs.parse()
