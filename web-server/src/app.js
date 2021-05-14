c24f04f48bc27177057a707765fe7d4d04c4da5f
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-14 11:59:29 -0300

    Serving up HTML and Json

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,30 @@
-const express = require('express');
-const app = express();
+const express = require('express')
 
-app.get('/', (req, res) => {
-    res.send('Hello express!');
-});
+const app = express()
+
+app.get('', (req, res) => {
+    res.send('<h1>Weather</h1>')
+})
 
 app.get('/help', (req, res) => {
-    res.send('Help page.');
-});
+    res.send([{
+        name: 'Andrew'
+    }, {
+        name: 'Sarah'
+    }])
+})
 
 app.get('/about', (req, res) => {
-    res.send('About page.');
-});
+    res.send('<h1>About</h1>')
+})
 
 app.get('/weather', (req, res) => {
-    res.send('Your Weather page.');
-});
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
+})
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000.');
-});
+    console.log('Server is up on port 3000.')
+})
