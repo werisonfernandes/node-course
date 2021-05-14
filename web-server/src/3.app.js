6052e6125cb910753d3134dc5dca91ca69b51b69
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-14 15:34:05 -0300

    Advanced template

--- web-server/src/3.app.js
+++ web-server/src/3.app.js
@@ -1,18 +1,15 @@
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const viewsPath = path.join(__dirname, '../templates')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -20,22 +17,20 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Werison Fernandes'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Werison Fernandes'
     })
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        helpText: 'This is some helpful text.',
-        title: 'Help',
-        name: 'Andrew Mead'
+        helpText: 'This is some helpful text.'
     })
 })
 
