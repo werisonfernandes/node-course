8064139017b61fcbff5d1982a305725e2e29be80
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-17 12:32:58 -0300

    Building a http endpoint

--- web-server/src/5.app.js
+++ web-server/src/5.app.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -48,22 +46,10 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    geocode(req.query.address, (error, { latitude, longitude, location }) => {
-        if (error) {
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia',
+        address: req.query.address
     })
 })
 
