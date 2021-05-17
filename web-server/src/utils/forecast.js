ce468cba55eb3d327ea49f2f059958304d07c296
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-17 14:53:56 -0300

    Fixing building a http endpoint

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    //const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/bd8adb13febf1f6e108c6d8d155ca373/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -1,7 +1,8 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoid2VyaXNvbi1mYXciLCJhIjoiY2tvbXplM2p0MDlyNzJub2k5eWZtN292aiJ9.hw-G6rDlArNcVlTgbj3jqQ&limit=1'
+    //const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
