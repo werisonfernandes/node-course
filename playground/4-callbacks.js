5fdc36931ca5a3fd2dacad0edb38d5b899254d01
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-05-21 17:23:23 -0300

    Promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -31,3 +31,18 @@ const add = (a, b, callback) => {
 add(1, 4, (sum) => {
     console.log(sum) // Should print: 5
 })
+
+const doWorkCallback = (callback) => {
+    setTimeout(() => {
+        // callback('This is my error!', undefined)
+        callback(undefined, [1, 4, 7])
+    }, 2000)
+}
+
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
+})
