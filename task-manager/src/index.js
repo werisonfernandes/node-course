0b72a32b249a4682273b64cc95fe9267ed58289d
Werison Aparecido Fernandes <werison.faw@gmail.com>
2021-06-08 21:42:02 -0300

    Hidding private data

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 3000
 //     }
 // })
 
-app.use((req, res, next) => {
-    res.status(503).send('Site is currently down. Check back soon!')
-})
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,15 +25,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days' })
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisismynewcourse')
-    console.log(data)
-}
-
-myFunction()
